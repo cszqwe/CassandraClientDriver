ddf473da58f525e2514dc5e14185af06caa03403
cszqwe <18322366@qq.com>
2016-10-13 15:47:20 -0400

    New kinds of test script

--- ClientDriver/d8_1_10.sh
+++ ClientDriver/d8_1_10.sh
@@ -1,2 +1,6 @@
- time java -cp target/my-app2-1.0-SNAPSHOT-jar-with-dependencies.jar  clientdriver.ClientDriver d8cas ~/cs4224/test.txt 127.0.0.1 >1.txt &
- time java -cp target/my-app2-1.0-SNAPSHOT-jar-with-dependencies.jar  clientdriver.ClientDriver d8cas ~/cs4224/test.txt 127.0.0.1 >2.txt &
+#!/bin/bash
+for i in {1..10}
+do
+	time java -cp target/my-app2-1.0-SNAPSHOT-jar-with-dependencies.jar  clientdriver.ClientDriver d8cas ~/cs4224/test2.txt 127.0.0.1 >$i.txt &
+#	java -cp target/my-app2-1.0-SNAPSHOT-jar-with-dependencies.jar  clientdriver.ClientDriver d8cas ~/cs4224/test.txt 127.0.0.1 >2.txt &
+done
